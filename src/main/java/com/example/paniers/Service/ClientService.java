1b632fa67c798e5162d0d2fc05d352603fe382f7
lamha <la210006@stuent.helha.be>
2024-08-06 17:10:01 +0200

    Ajout des services pour Client et Reservation

--- src/main/java/com/example/paniers/Service/ClientService.java
+++ src/main/java/com/example/paniers/Service/ClientService.java
@@ -1,4 +1,30 @@
 package com.example.paniers.Service;
 
+import com.example.paniers.Dao.ClientDao;
+import com.example.paniers.Models.Client;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ClientService {
+    @Autowired
+    private ClientDao clientDao;
+
+    public List<Client> getAllClients() {
+        return clientDao.findAll();
+    }
+
+    public void addClient(Client client) {
+        clientDao.save(client);
+    }
+
+    public Client getClientById(Long id) {
+        return clientDao.findById(id).orElse(null);
+    }
+
+    public void deleteClient(Long id) {
+        clientDao.deleteById(id);
+    }
 }
