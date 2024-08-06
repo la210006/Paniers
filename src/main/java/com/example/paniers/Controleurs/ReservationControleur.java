31ad19d6cab572442d73f2250abf809e18c7cbf5
lamha <la210006@stuent.helha.be>
2024-08-06 16:38:37 +0200

    Dev des crud pour les réservations

--- src/main/java/com/example/paniers/Controleurs/ReservationControleur.java
+++ src/main/java/com/example/paniers/Controleurs/ReservationControleur.java
@@ -1,4 +1,35 @@
 package com.example.paniers.Controleurs;
 
+import com.example.paniers.Models.Reservation;
+import com.example.paniers.Service.ReservationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/reservations")
 public class ReservationControleur {
+    @Autowired
+    private ReservationService reservationService;
+
+    @GetMapping
+    public List<Reservation> getAllReservations() {
+        return reservationService.getAllReservations();
+    }
+
+    @PostMapping
+    public void addReservation(@RequestBody Reservation reservation) {
+        reservationService.addReservation(reservation);
+    }
+
+    @GetMapping("/{id}")
+    public Reservation getReservationById(@PathVariable Long id) {
+        return reservationService.getReservationById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteReservation(@PathVariable Long id) {
+        reservationService.deleteReservation(id);
+    }
 }
