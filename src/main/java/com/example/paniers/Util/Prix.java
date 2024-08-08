38d37e60a3e900ef6cc80805be1188943250a332
lamha <la210006@stuent.helha.be>
2024-08-08 21:16:46 +0200

    Après verification la lecture du prix ne venait pas du .json donc des modification pour pouvoir le récuperer du .json

--- src/main/java/com/example/paniers/Util/Prix.java
+++ src/main/java/com/example/paniers/Util/Prix.java
@@ -1,37 +1,40 @@
 package com.example.paniers.Util;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
 
 public class Prix {
-    private static final String PRIX_FILE = "src/main/resources/prix.json";
-    private static Prix instance;
-    private int prixPanier;
+    private static final Logger logger = LoggerFactory.getLogger(Prix.class);
+    private double prixPanier;
 
-    private Prix() {
-        ObjectMapper mapper = new ObjectMapper();
-        try {
-            Prix prix = mapper.readValue(new File(PRIX_FILE), Prix.class);
-            this.prixPanier = prix.getPrixPanier();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static synchronized Prix getInstance() {
-        if (instance == null) {
-            instance = new Prix();
-        }
-        return instance;
-    }
-
-    public int getPrixPanier() {
+    public double getPrixPanier() {
         return prixPanier;
     }
 
-    public void setPrixPanier(int prixPanier) {
+    public void setPrixPanier(double prixPanier) {
         this.prixPanier = prixPanier;
+    }
+
+    public static Prix lirePrixDuFichier(String cheminFichier) {
+        ObjectMapper mapper = new ObjectMapper();
+        Prix prix = null;
+        try {
+            File fichier = new File(cheminFichier);
+            logger.info("Chemin du fichier: " + fichier.getAbsolutePath());
+            if (fichier.exists()) {
+                logger.info("Le fichier existe: " + fichier.getAbsolutePath());
+                prix = mapper.readValue(fichier, Prix.class);
+                logger.info("Prix lu: " + prix.getPrixPanier());
+            } else {
+                logger.error("Le fichier n'existe pas: " + fichier.getAbsolutePath());
+            }
+        } catch (IOException e) {
+            logger.error("Erreur lors de la lecture du fichier JSON", e);
+        }
+        return prix;
     }
 }
