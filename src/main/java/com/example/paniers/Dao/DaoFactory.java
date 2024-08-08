cffcb248974e6d8b7c13b8043b0314a360e06a1e
lamha <la210006@stuent.helha.be>
2024-08-08 21:21:56 +0200

    DaoFactory pour aller lire le prix a partir du .json

--- src/main/java/com/example/paniers/Dao/DaoFactory.java
+++ src/main/java/com/example/paniers/Dao/DaoFactory.java
@@ -1,7 +1,12 @@
 package com.example.paniers.Dao;
 
+import com.example.paniers.Util.Prix;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 
 @Component
 public class DaoFactory {
@@ -12,11 +17,30 @@ public class DaoFactory {
     @Autowired
     private ReservationDao reservationDao;
 
+    @Autowired
+    private ResourceLoader resourceLoader;
+
+    private static final String CHEMIN_FICHIER_PRIX = "classpath:prix.json";
+
     public ClientDao getClientDao() {
         return clientDao;
     }
 
     public ReservationDao getReservationDao() {
         return reservationDao;
+    }
+
+    public double getPrixUnitaire() {
+        Resource resource = resourceLoader.getResource(CHEMIN_FICHIER_PRIX);
+        try {
+            Prix prix = Prix.lirePrixDuFichier(resource.getFile().getAbsolutePath());
+            if (prix != null) {
+                return prix.getPrixPanier();
+            } else {
+                throw new RuntimeException("Impossible de lire le prix depuis le fichier JSON.");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Erreur lors de l'accès au fichier JSON.", e);
+        }
     }
 }
