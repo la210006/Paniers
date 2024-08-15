adbdd34da54899709976941b51250e145468182a
lamha <la210006@stuent.helha.be>
2024-08-15 17:53:12 +0200

    Modification pour chercher le prix du .json

--- src/main/java/com/example/paniers/Service/ReservationService.java
+++ src/main/java/com/example/paniers/Service/ReservationService.java
@@ -2,6 +2,7 @@ package com.example.paniers.Service;
 
 import com.example.paniers.Dao.ReservationDao;
 import com.example.paniers.Models.Reservation;
+import com.example.paniers.Util.Prix;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +18,13 @@ public class ReservationService {
     }
 
     public void addReservation(Reservation reservation) {
+        // Lire le prix du fichier JSON
+        Prix prix = Prix.lirePrixDuFichier("Prix.json");
+        if (prix != null) {
+            double prixUnitaire = prix.getPrixPanier();
+            reservation.setPrixUnitaire(prixUnitaire);
+            reservation.setPrix(prixUnitaire * reservation.getQuantite());
+        }
         reservationDao.save(reservation);
     }
 
